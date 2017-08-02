0c66ea2b9fa12c6ee7b609f8ea263c7c3877c978
Baptiste FROCHOT <bfrochot@student.42.fr>
2017-08-02 20:31:10 +0200

    Rien

--- palloc.c
+++ palloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   palloc.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bfrochot <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: bfrochot <bfrochot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/12 14:46:50 by bfrochot          #+#    #+#             */
-/*   Updated: 2017/07/12 14:46:52 by bfrochot         ###   ########.fr       */
+/*   Updated: 2017/08/02 15:37:05 by bfrochot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*palloc(int i)
 	s = malloc(i);
 	if (!s)
 	{
-		ft_putstr("Malloc error\n");
+		ft_putstr_fd("Malloc error\n", 2);
 		exit(1);
 	}
 	return (s);
